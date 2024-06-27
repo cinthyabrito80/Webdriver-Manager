22f6ae3e4f3d92060f89ffc5dcb019c0654b9a23
cinthyabrito80 <cinthya_brito@yahoo.com.br>
2024-06-27 20:01:42 -0300

    ajuste na lib

--- src/test/java/br/com/cinth/EstudoTest.java
+++ src/test/java/br/com/cinth/EstudoTest.java
@@ -8,7 +8,6 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 //CLASSE (CLASS)
